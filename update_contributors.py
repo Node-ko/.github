affabed0d65b1f0aa1dc3c3c640e9105aae1318c
맛김 <tastekim@kakao.com>
2024-12-03 17:07:44 +0900

    Update update_contributors.py

--- update_contributors.py
+++ update_contributors.py
@@ -44,7 +44,7 @@ def generate_contributors_markdown(contributors, max_contributors=100, per_row=1
     for contributor in contributors:
         markdown += f'''
         <a href="{contributor["html_url"]}" target="_blank">
-          <img src="{contributor["avatar_url"]}" alt="{contributor["login"]}" style="vertical-align: middle; width: 64px; height: 64px; border-radius: 50%"/>
+          <img src="{contributor["avatar_url"]}" alt="{contributor["login"]}" style="vertical-align: middle; width: 64px; height: 64px; max-width: 64px; max-height: 64px; border-radius: 50%"/>
         </a>'''
 
     markdown += '''</div>
