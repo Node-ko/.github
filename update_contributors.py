5520907af13585f2ea1dafcd53b95c265621afbb
맛김 <tastekim@kakao.com>
2024-12-12 12:37:47 +0900

    Update update_contributors.py

--- update_contributors.py
+++ update_contributors.py
@@ -29,28 +29,6 @@ def fetch_contributors():
     response.raise_for_status()
     return response.json()
 
-def generate_contributors_markdown(contributors, max_contributors=100, per_row=12):
-    # 최대 기여자 수 제한
-    contributors = contributors[:max_contributors]
-    
-    # 한 줄에 표시할 기여자 수
-    per_row = per_row
-
-    # Calculate the width percentage for each contributor
-    width_percent = 100 / per_row
-
-    markdown = '''<div>
-        <div style="width: {width_percent}%; padding: 5px; box-sizing: border-box;">'''
-    for contributor in contributors:
-        markdown += f'''
-        <a href="{contributor["html_url"]}" target="_blank">
-          <img src="{contributor["avatar_url"]}" alt="{contributor["login"]}" style="vertical-align: middle; width: 64px; height: 64px; max-width: 64px; max-height: 64px; border-radius: 50%"/>
-        </a>'''
-
-    markdown += '''</div>
-    </div>'''
-    return markdown
-
 def update_readme(contributors_markdown):
     with open(README_PATH, 'r', encoding='utf-8') as file:
         readme_content = file.read()
@@ -72,7 +50,11 @@ def main():
         return
 
     # 최대 100명, 한 줄에 12명씩 표시
-    contributors_markdown = generate_contributors_markdown(contributors, max_contributors=100, per_row=12)
+    contributors_markdown = '''
+    <a href="https://github.com/Node-ko/learn/graphs/contributors">
+      <img src="https://contrib.rocks/image?repo=Node-ko/learn" />
+    </a>
+    '''
     update_readme(contributors_markdown)
     print("README.md has been updated with the latest contributors.")
 
